41716ed7f6343bd60d90727d3abc61e5565e0a27
Delvio <elvioseverino@gmail.com>
2022-12-08 11:20:42 -0500

    fixed search from product detail

--- js/search_button.js
+++ js/search_button.js
@@ -1,5 +1,5 @@
 let button = document.querySelector('.search-icon-container');
 let searchField = document.querySelector('.search-field');
 button.addEventListener('click', () => {
-  window.location.href = `search.html?search=${searchField.value}`;
+  window.location.href = `/search.html?search=${searchField.value}`;
 });
